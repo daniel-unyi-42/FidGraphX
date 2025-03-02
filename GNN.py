270bc802213e20400920197e2bc5dc1c6e88e6ec
Daniel Unyi <daniel.unyi.42@gmail.com>
2025-03-02 12:35:13 +0000

    predictor loss calculated together with whole graphs and consistency loss

--- GNN.py
+++ GNN.py
@@ -143,21 +143,19 @@ class GNN(nn.Module):
         if type(self).__name__ == 'Predictor':
           logits_full = self(data, mask=torch.ones((data.num_nodes, 1), device=data.x.device))
           loss_full = self.criterion(logits_full, data.y)
-          # consistency_loss = (1 - F.cosine_similarity(logits, logits_full)).mean()
-          loss = loss + loss_full# + consistency_loss
+          consistency_loss = (1 - F.cosine_similarity(logits, logits_full)).mean()
+          loss = loss + loss_full + consistency_loss
 
 
-
-
-
-
-        # # predictor model randomly masks a predefined ratio of nodes in each graph
-        # # here, we add loss for multiple augmented samples
         # if type(self).__name__ == 'Predictor':
         #   for _ in range(aug_size):
         #     logits_other = self(data)
         #     loss_other = self.criterion(logits_other, data.y)
         #     loss = loss + loss_other
+
+
+
+
 
         # # VERSION 1: ADD AUG LOSS
         # if type(self).__name__ == 'Predictor':
