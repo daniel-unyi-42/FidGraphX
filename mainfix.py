5885d9663c3f518c7c62d4ded25a4b46e6ba5fdb
Daniel Unyi <daniel.unyi.42@gmail.com>
2025-06-21 10:14:10 +0000

    Improved logging

--- mainfix.py
+++ mainfix.py
@@ -1,5 +1,6 @@
 import yaml
 import os
+import logging
 import time
 import numpy as np
 import networkx as nx
@@ -20,7 +21,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from tensorboardX import SummaryWriter
 
-
 # load config yaml
 with open('config.yaml') as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
@@ -30,23 +30,30 @@ seed = config['seed']
 torch.manual_seed(seed)
 np.random.seed(seed)
 
-# set data path and log path
-data_path = f"{config['data_path']}/{config['dataset']}"
-os.makedirs(data_path, exist_ok=True)
-log_path_base = f"{config['log_path']}/{config['dataset']}"
-os.makedirs(log_path_base, exist_ok=True)
-
-
 # initialize logger
+log_path_base = f"{config['log_path']}/{config['dataset']}"
 run_id = str(int(time.time()))
 log_path = os.path.join(log_path_base, f'run_{run_id}')
+os.makedirs(log_path, exist_ok=True)
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.FileHandler(os.path.join(log_path, 'log.txt')),
+        logging.StreamHandler()
+    ]
+)
+logging.getLogger('matplotlib').setLevel(logging.WARNING)
 writer = SummaryWriter(log_path)
 with open(f'{log_path}/config.yaml', 'w') as f:
     yaml.dump(config, f)
 
 # load dataset
+data_path = f"{config['data_path']}/{config['dataset']}"
+os.makedirs(data_path, exist_ok=True)
+
 if config['dataset'] == 'BAMotifs':
-    dataset = BAMotifs(data_path, num_graphs=500, attach_prob=0.2)
+    dataset = BAMotifs(data_path, num_graphs=3000, attach_prob=0.2)
 
 elif config['dataset'] == 'BAMotifsVolume':
     dataset = BAMotifsVolumeDataset(data_path, num_graphs=500, ba_nodes=25, attach_prob=0.1)
@@ -73,7 +80,7 @@ else:
     raise ValueError(f"Dataset {config['dataset']} not supported")
 
 dataset = dataset.shuffle()
-print(f'Dataset {config["dataset"]} loaded, number of graphs: {len(dataset)}')
+logging.info(f'Dataset {config["dataset"]} loaded, number of graphs: {len(dataset)}')
 
 # split dataset
 train_set, val_set, test_set = torch.utils.data.random_split(
@@ -87,7 +94,7 @@ val_loader = DataLoader(val_set, batch_size=len(val_set))
 test_loader = DataLoader(test_set, batch_size=len(test_set))
 
 expected_sparsity = np.array([data.true.sum().item() / data.num_nodes for data in train_loader]).mean()
-print('Expected sparsity: ', expected_sparsity)
+logging.info(f"Expected sparsity: {expected_sparsity}")
 
 num_node_features = dataset[0].x.shape[1]
 num_edge_features = 0 if dataset[0].edge_attr is None else dataset[0].edge_attr.shape[1]
@@ -106,7 +113,7 @@ baseline = GNN(
     use_norm=config['use_norm'],
 )
 
-# print('Baseline model:\n', baseline)
+# logging.info('Baseline model:\n', baseline)
 
 baseline_pretrained = config['baseline_pretrained']
 
@@ -119,11 +126,11 @@ if baseline_pretrained is None:
         if (config['task_type'] == 'classification' and best_val_acc < val_acc) or \
            (config['task_type'] == 'regression' and best_val_acc > val_acc):
             best_val_acc = val_acc
-            print(f'Best validation accuracy updated: {best_val_acc:.4f}, saving baseline...')
+            logging.info(f'Best validation accuracy updated: {best_val_acc:.4f}, saving baseline...')
             torch.save(baseline.state_dict(), os.path.join(log_path, 'baseline.pt'))
-        if config['print_results']:
-            print(f'Epoch: {epoch}, Train loss: {train_loss:.4f}, Train acc: {train_acc:.4f}')
-            print(f'Epoch: {epoch}, Val loss: {val_loss:.4f}, Val acc: {val_acc:.4f}')
+        if config['logging']:
+            logging.info(f'Epoch: {epoch}, Train loss: {train_loss:.4f}, Train acc: {train_acc:.4f}')
+            logging.info(f'Epoch: {epoch}, Val loss: {val_loss:.4f}, Val acc: {val_acc:.4f}')
         writer.add_scalar('BASELINE/train_loss', train_loss, epoch)
         writer.add_scalar('BASELINE/train_acc', train_acc, epoch)
         writer.add_scalar('BASELINE/val_loss', val_loss, epoch)
@@ -133,9 +140,9 @@ else:
     baseline_path_pretrained = os.path.join(log_path_base, baseline_pretrained)
     baseline.load_state_dict(torch.load(os.path.join(baseline_path_pretrained, 'baseline.pt'), weights_only=False))
 val_loss, val_acc = baseline.test_batch(val_loader)
-print(f'Val loss: {val_loss:.4f}, Val acc: {val_acc:.4f}')
+logging.info(f'Val loss: {val_loss:.4f}, Val acc: {val_acc:.4f}')
 test_loss, test_acc = baseline.test_batch(test_loader)
-print(f'Test loss: {test_loss:.4f}, Test acc: {test_acc:.4f}')
+logging.info(f'Test loss: {test_loss:.4f}, Test acc: {test_acc:.4f}')
 
 # plot confusion matrix
 if config['task_type'] == 'classification':
@@ -192,7 +199,7 @@ explainer = Explainer(
     reward_coeff=config['reward_coeff'],
 )
 
-# print('Explainer model:\n', explainer)
+# logging.info('Explainer model:\n', explainer)
 
 explainer_pretrained = config['explainer_pretrained']
 
@@ -207,17 +214,17 @@ if explainer_pretrained is None:
             train_exp = not train_exp
         # if epoch % 50 == 0 and explainer.sparsity > max(config['sparsity'], 0.05):
         #     explainer.sparsity -= 0.05
-        #     print(f'Sparsity updated: {explainer.sparsity:.4f}')
+        #     logging.info(f'Sparsity updated: {explainer.sparsity:.4f}')
         train_loss, train_sparsity, train_pos_loss, train_neg_loss, train_pos_metric, train_neg_metric, train_fid_plus_probs, train_fid_minus_probs, train_fid_plus_acc, train_fid_minus_acc, train_auc, train_precision, train_recall, train_iou = explainer.train_batch(train_loader, train_pred=True, train_exp=True)
         val_loss, val_sparsity, val_pos_loss, val_neg_loss, val_pos_metric, val_neg_metric, val_fid_plus_probs, val_fid_minus_probs, val_fid_plus_acc, val_fid_minus_acc, val_auc, val_precision, val_recall, val_iou = explainer.test_batch(val_loader)
         val_fidelity_diff = val_fid_plus_probs - val_fid_minus_probs
         if val_fidelity_diff > best_val_fidelity_diff and val_sparsity < config['sparsity']:
             best_val_fidelity_diff = val_fidelity_diff
-            print(f'Best validation fidelity difference updated: {best_val_fidelity_diff:.4f}, saving explainer...')
+            logging.info(f'Best validation fidelity difference updated: {best_val_fidelity_diff:.4f}, saving explainer...')
             torch.save(explainer.state_dict(), os.path.join(log_path, 'explainer.pt'))
-        if config['print_results']:
-            print(f'Epoch: {epoch}, Train loss: {train_loss:.4f}, Train sparsity: {train_sparsity:.4f}, Train pos loss: {train_pos_loss:.4f}, Train neg loss: {train_neg_loss:.4f}, Train pos metric: {train_pos_metric:.4f}, Train neg metric: {train_neg_metric:.4f}, Train fid plus probs: {train_fid_plus_probs:.4f}, Train fid minus probs: {train_fid_minus_probs:.4f}, Train fid plus acc: {train_fid_plus_acc:.4f}, Train fid minus acc: {train_fid_minus_acc:.4f}, Train auc: {train_auc:.4f}, Train precision: {train_precision:.4f}, Train recall: {train_recall:.4f}, Train iou: {train_iou:.4f}')
-            print(f'Epoch: {epoch}, Val loss: {val_loss:.4f}, Val sparsity: {val_sparsity:.4f}, Val pos loss: {val_pos_loss:.4f}, Val neg loss: {val_neg_loss:.4f}, Val pos metric: {val_pos_metric:.4f}, Val neg metric: {val_neg_metric:.4f}, Val fid plus probs: {val_fid_plus_probs:.4f}, Val fid minus probs: {val_fid_minus_probs:.4f}, Val fid plus acc: {val_fid_plus_acc:.4f}, Val fid minus acc: {val_fid_minus_acc:.4f}, Val auc: {val_auc:.4f}, Val precision: {val_precision:.4f}, Val recall: {val_recall:.4f}, Val iou: {val_iou:.4f}')
+        if config['logging']:
+            logging.info(f'Epoch: {epoch}, Train loss: {train_loss:.4f}, Train sparsity: {train_sparsity:.4f}, Train pos loss: {train_pos_loss:.4f}, Train neg loss: {train_neg_loss:.4f}, Train pos metric: {train_pos_metric:.4f}, Train neg metric: {train_neg_metric:.4f}, Train fid plus probs: {train_fid_plus_probs:.4f}, Train fid minus probs: {train_fid_minus_probs:.4f}, Train fid plus acc: {train_fid_plus_acc:.4f}, Train fid minus acc: {train_fid_minus_acc:.4f}, Train auc: {train_auc:.4f}, Train precision: {train_precision:.4f}, Train recall: {train_recall:.4f}, Train iou: {train_iou:.4f}')
+            logging.info(f'Epoch: {epoch}, Val loss: {val_loss:.4f}, Val sparsity: {val_sparsity:.4f}, Val pos loss: {val_pos_loss:.4f}, Val neg loss: {val_neg_loss:.4f}, Val pos metric: {val_pos_metric:.4f}, Val neg metric: {val_neg_metric:.4f}, Val fid plus probs: {val_fid_plus_probs:.4f}, Val fid minus probs: {val_fid_minus_probs:.4f}, Val fid plus acc: {val_fid_plus_acc:.4f}, Val fid minus acc: {val_fid_minus_acc:.4f}, Val auc: {val_auc:.4f}, Val precision: {val_precision:.4f}, Val recall: {val_recall:.4f}, Val iou: {val_iou:.4f}')
         writer.add_scalar('EXPLAINER/train_loss', train_loss, epoch)
         writer.add_scalar('EXPLAINER/train_sparsity', train_sparsity, epoch)
         writer.add_scalar('EXPLAINER/train_pos_loss', train_pos_loss, epoch)
@@ -253,8 +260,8 @@ else:
 
 val_loss, val_sparsity, val_pos_loss, val_neg_loss, val_pos_metric, val_neg_metric, val_fid_plus_probs, val_fid_minus_probs, val_fid_plus_acc, val_fid_minus_acc, val_auc, val_precision, val_recall, val_iou = explainer.test_batch(val_loader)
 test_loss, test_sparsity, test_pos_loss, test_neg_loss, test_pos_metric, test_neg_metric, test_fid_plus_probs, test_fid_minus_probs, test_fid_plus_acc, test_fid_minus_acc, test_auc, test_precision, test_recall, test_iou = explainer.test_batch(test_loader)
-print(f'Val loss: {val_loss:.4f}, Val sparsity: {val_sparsity:.4f}, Val pos loss: {val_pos_loss:.4f}, Val neg loss: {val_neg_loss:.4f}, Val pos metric: {val_pos_metric:.4f}, Val neg metric: {val_neg_metric:.4f}, Val fid plus probs: {val_fid_plus_probs:.4f}, Val fid minus probs: {val_fid_minus_probs:.4f}, Val fid plus acc: {val_fid_plus_acc:.4f}, Val fid minus acc: {val_fid_minus_acc:.4f}, Val auc: {val_auc:.4f}, Val precision: {val_precision:.4f}, Val recall: {val_recall:.4f}, Val iou: {val_iou:.4f}')
-print(f'Test loss: {test_loss:.4f}, Test sparsity: {test_sparsity:.4f}, Test pos loss: {test_pos_loss:.4f}, Test neg loss: {test_neg_loss:.4f}, Test pos metric: {test_pos_metric:.4f}, Test neg metric: {test_neg_metric:.4f}, Test fid plus probs: {test_fid_plus_probs:.4f}, Test fid minus probs: {test_fid_minus_probs:.4f}, Test fid plus acc: {test_fid_plus_acc:.4f}, Test fid minus acc: {test_fid_minus_acc:.4f}, Test auc: {test_auc:.4f}, Test precision: {test_precision:.4f}, Test recall: {test_recall:.4f}, Test iou: {test_iou:.4f}')
+logging.info(f'Val loss: {val_loss:.4f}, Val sparsity: {val_sparsity:.4f}, Val pos loss: {val_pos_loss:.4f}, Val neg loss: {val_neg_loss:.4f}, Val pos metric: {val_pos_metric:.4f}, Val neg metric: {val_neg_metric:.4f}, Val fid plus probs: {val_fid_plus_probs:.4f}, Val fid minus probs: {val_fid_minus_probs:.4f}, Val fid plus acc: {val_fid_plus_acc:.4f}, Val fid minus acc: {val_fid_minus_acc:.4f}, Val auc: {val_auc:.4f}, Val precision: {val_precision:.4f}, Val recall: {val_recall:.4f}, Val iou: {val_iou:.4f}')
+logging.info(f'Test loss: {test_loss:.4f}, Test sparsity: {test_sparsity:.4f}, Test pos loss: {test_pos_loss:.4f}, Test neg loss: {test_neg_loss:.4f}, Test pos metric: {test_pos_metric:.4f}, Test neg metric: {test_neg_metric:.4f}, Test fid plus probs: {test_fid_plus_probs:.4f}, Test fid minus probs: {test_fid_minus_probs:.4f}, Test fid plus acc: {test_fid_plus_acc:.4f}, Test fid minus acc: {test_fid_minus_acc:.4f}, Test auc: {test_auc:.4f}, Test precision: {test_precision:.4f}, Test recall: {test_recall:.4f}, Test iou: {test_iou:.4f}')
 
 y_probs, y_masks, explanations, pos_preds, neg_preds, baseline_preds, y_trues = explainer.predict_batch(test_loader)
 
