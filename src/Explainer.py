Fixing bug in metric calculation; using iou instead of ami score

src/Explainer.py
@@ -9,7 +9,7 @@ from src.metrics import (
     fid_plus_class, fid_minus_class,
     fid_minus_reg, fid_plus_reg,
     precision_score, recall_score, f1_score,
-    auc_score, ami_score
+    auc_score, iou_score
 )
 
 class Explainer(nn.Module):
@@ -98,7 +98,7 @@ class Explainer(nn.Module):
             'precision': 0.0,
             'recall': 0.0,
             'f1': 0.0,
-            'ami': 0.0
+            'iou': 0.0
         }
         for data in loader:
             data = data.to(self.device)
@@ -167,7 +167,7 @@ class Explainer(nn.Module):
                 metrics['precision'] += precision_score(mask, data.true)
                 metrics['recall'] += recall_score(mask, data.true)
                 metrics['f1'] += f1_score(mask, data.true)
-                metrics['ami'] += ami_score(mask, data.true)
+                metrics['iou'] += iou_score(mask, data.true)
         for metric_name in metrics:
             metrics[metric_name] /= len(loader)
         return metrics
@@ -193,7 +193,7 @@ class Explainer(nn.Module):
             'precision': 0.0,
             'recall': 0.0,
             'f1': 0.0,
-            'ami': 0.0
+            'iou': 0.0
         }
         for data in loader:
             data = data.to(self.device)
@@ -230,7 +230,7 @@ class Explainer(nn.Module):
                 metrics['precision'] += precision_score(mask, data.true)
                 metrics['recall'] += recall_score(mask, data.true)
                 metrics['f1'] += f1_score(mask, data.true),
-                metrics['ami'] += ami_score(mask, data.true)
+                metrics['iou'] += iou_score(mask, data.true)
         for metric_name in metrics:
             metrics[metric_name] /= len(loader)
         return metrics